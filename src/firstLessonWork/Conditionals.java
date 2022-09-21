a798fec1382b6511b3449f9290422eae396b991e
zehra.keles <zehra.keles@LAPTOP-6Q4IF77I>
2022-09-21 17:15:05 +0300

    java camp first lesson homework

--- src/firstLessonWork/Conditionals.java
+++ src/firstLessonWork/Conditionals.java
@@ -4,7 +4,7 @@ public class Conditionals {
     public static void main(String[] args) {
         int number =27;
         if(number < 30){
-            System.out.println("number less than 30 ");
+            System.out.println("number less than 60 ");
         } else if (number == 20) {
             System.out.println("number equal to 20 ");
         }else{
